5a5aac39f652ddb1c86baef3a6c6f4816aca46e9
devcihat <cihattsungu@outlook.com>
2021-05-20 14:47:38 +0300

    up

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MainStackNavigator from "./src/navigators/MainStackNavigator";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStackNavigator />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/components/home/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box , Text} from 'react-native-design-utility'
+
+const HomeScreen = () => {
+    return (
+        <Box f={1} center>
+            <Text>Home Screen</Text>
+        </Box>
+    )
+}
+
+
+export default HomeScreen
