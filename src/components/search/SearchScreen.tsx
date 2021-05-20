5c4dfa94ffb6f6caf8e160358af06862b40ce326
devcihat <cihattsungu@outlook.com>
2021-05-20 17:22:49 +0300

    up

--- src/components/search/SearchScreen.tsx
+++ src/components/search/SearchScreen.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { Box , Text} from 'react-native-design-utility'
+import { Box} from 'react-native-design-utility'
+import { StyleSheet, TextInput } from 'react-native';
+
 
 const SearchScreen = () => {
     return (
-        <Box f={1} center>
-            <Text>SearchScreen Screen</Text>
+        <Box f={1} bg='white'>
+            <Box h={50} w="100%" px='sm' mt='sm'>
+                <TextInput selectionColor='blueLight' placeholder="Search Podcast" style={styles.input} />
+            </Box>
         </Box>
     )
 }
 
+
+const styles = StyleSheet.create({
+    input: {
+        height:40,
+        flex:1,
+        backgroundColor:'#dddddd',
+        borderRadius:10,
+        paddingHorizontal:10,
+        fontSize:15
+    }
+})
 
 export default SearchScreen
